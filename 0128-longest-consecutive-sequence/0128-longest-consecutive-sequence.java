ce1ac0b12120f7983a21be6820eca5de5dfb1324
Gaurav Kumar <160252955+KumarGaurav007@users.noreply.github.com>
2026-09-07 21:37:59 +0530

    Time: 31 ms (58.93%), Space: 96 MB (5.93%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,56 +1,26 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        // Approach 1 - sorting
-        // Arrays.sort(nums);
-        // int maxCount = 1;
-        // int count = 1;
-
-        // if(nums.length == 0) return 0;
-
-        // for(int i=1; i<nums.length; i++){
-        //     if(nums[i] - nums[i-1] == 1){
-        //         count++;
-        //     }
-        //     else if(nums[i] - nums[i-1] == 0){
-
-        //     }
-        //     else if(nums[i] - nums[i-1] > 1){
-        //         maxCount = Math.max(maxCount, count);
-        //         count = 1;
-        //     }
-        // }
-        // maxCount = Math.max(maxCount, count);
-
-        // return maxCount;
-
-        // Approch 2 - using set without sorting
-
-        if (nums.length == 0) return 0;
-
+        
+        int maxcount=0;
         HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
+
+        for(int num : nums){
             set.add(num);
         }
 
-        int maxCount = 0;
-
-        for (int num : set) {
-
-            if (!set.contains(num - 1)) {
-
-                int current = num;
+        for(int num : set){
+            int start=0;
+            if(! set.contains(num-1)){
+                start = num;
                 int count = 1;
-
-                while (current != Integer.MAX_VALUE &&
-                       set.contains(current + 1)) {
-                    current++;
-                    count++;
+                while(set.contains(start+1)){
+                    count++ ;
+                    start ++;
                 }
-
-                maxCount = Math.max(maxCount, count);
+                maxcount = Math.max(count,maxcount);
+                count=0;
             }
         }
-
-        return maxCount;
+        return maxcount;
     }
 }
