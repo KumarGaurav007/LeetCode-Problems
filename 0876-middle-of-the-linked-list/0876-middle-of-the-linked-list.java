a5915594d95f27d36967e762b2575b7428c9f22c
Gaurav Kumar <160252955+KumarGaurav007@users.noreply.github.com>
2026-09-15 23:47:31 +0530

    Time: 0 ms (100%), Space: 42.8 MB (53%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,31 +10,13 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        /*// brute force approach 01
-        int len = 0;
-        ListNode temp = head;
-        while(temp != null){
-            temp = temp.next;
-            len++;
-        }
-        int mid = len/2;
-        temp = head;
-        for(int i=0; i<=mid; i++){
-            temp= temp.next;
-        }
-        return temp;*/
-
-        // approach 02 - slow fast pointer
-
-        ListNode temp = head;
-        ListNode fast = head;
         ListNode slow = head;
+        ListNode fast = head;
 
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
         return slow;
-        
     }
 }
